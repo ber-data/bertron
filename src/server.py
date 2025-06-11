ab7d1baac2f88f6fe7368a9833b1f9de261735d1
eecavanna <eecavanna@users.noreply.github.com>
2025-06-11 13:36:55 -0700

    Add Mongo service to container-based development environment

--- src/server.py
+++ src/server.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.responses import RedirectResponse
+from pymongo import MongoClient
+
+# Connect to MongoDB.
+# TODO: Get these values from environment variables instead of hard-coding them.
+mongo_client = MongoClient("mongo:27017", username="admin", password="root")
 
 app = FastAPI()
 
@@ -14,7 +19,16 @@ def get_root():
 @app.get("/health")
 def get_health():
     r"""Get API health information."""
-    return {"web_server": "ok", "database": None}
+    is_database_healthy = len(mongo_client.list_database_names()) > 0
+    return {"web_server": "ok", "database": is_database_healthy}
+
+
+# TODO: Delete this endpoint once we're confident in our database connection.
+@app.get("/experimental/database_names")
+def get_database_names():
+    r"""Get the names of all databases in the MongoDB server."""
+    db_names = mongo_client.list_database_names()
+    return {"database_names": db_names}
 
 
 if __name__ == "__main__":
