cb9bd7d03dcecaa76a6de08c385f2fd777594677
eecavanna <eecavanna@users.noreply.github.com>
2025-06-11 12:55:28 -0700

    Use `ruff format` to format Python code

--- src/server.py
+++ src/server.py
@@ -4,15 +4,18 @@ from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
+
 @app.get("/", include_in_schema=False)
 def get_root():
     r"""Redirect to the API documentation page."""
     return RedirectResponse(url="/docs")
 
+
 @app.get("/health")
 def get_health():
     r"""Get API health information."""
     return {"web_server": "ok", "database": None}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
