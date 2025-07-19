b5a4452085e69bdb281e36d3340d1d208436266e
eecavanna <eecavanna@users.noreply.github.com>
2025-07-18 17:44:50 -0700

    Add test that confirms `/version` returns a `VersionResponse`

--- src/tests/test_server.py
+++ src/tests/test_server.py
@@ -5,13 +5,30 @@ You can learn about testing FastAPI apps here:
 https://fastapi.tiangolo.com/tutorial/testing/
 """
 
+import pytest
 from fastapi.testclient import TestClient
 from starlette import status
+
+from models import VersionResponse
 from server import app
 
 
-def test_root_endpoint_redirects_to_api_docs():
-    client = TestClient(app)
-    response = client.get("/", follow_redirects=False)
+@pytest.fixture
+def test_client():
+    test_client = TestClient(app)
+    yield test_client
+
+
+def test_root_endpoint_redirects_to_api_docs(test_client: TestClient):
+    response = test_client.get("/", follow_redirects=False)
     assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
     assert response.headers["location"] == "/docs"
+
+
+def test_version_endpoint_returns_version_response(test_client: TestClient):
+    response = test_client.get("/version")
+    assert response.status_code == status.HTTP_200_OK
+    json_response = response.json()
+    expected_fields = set(VersionResponse.model_fields.keys())
+    actual_fields = set(json_response.keys())
+    assert actual_fields == expected_fields
