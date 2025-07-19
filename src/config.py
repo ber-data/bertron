cfc64e5167e803365d29d6febc305d693889ac54
eecavanna <134325062+eecavanna@users.noreply.github.com>
2025-07-19 16:47:16 -0700

    Read MongoDB connection parameters from environment variables (#76)
    On this branch, I made changes related to Docker, environment variables,
    and testing.
    
    ### Main changes
    - **Environment variable-based configuration**: I updated the server to
    use Pydantic's `pydantic-settings` package (documented
    [here](https://docs.pydantic.dev/latest/concepts/pydantic_settings/#the-basics),
    and also used by `microbiomedata/nmdc-server`) to get the MongoDB
    connection parameters from environment variables. The precedence order
    (from highest to lowest) is: OS, `.env` file, class attribute defaults.
    Fixes #53.
    - **Example `.env` file**: I included an example `.env` file named
    `.env.example` and instructions on creating one's own `.env` file.
    Here's an example `.env` file:
      ```ini
      MONGO_HOST=mongo
      MONGO_PORT=27017
      MONGO_DATABASE=bertron
      MONGO_USERNAME=admin
      MONGO_PASSWORD=root
      ```
    - **Name of test database**: I added a pytest fixture that automatically
    sets the test database name in the context of all tests, overriding
    whatever the user might have in their `.env` file. The test database is
    always named `bertron_test` (we can make this, too, be read from an
    environment variable in a future PR). As a protective measure, tests
    will not run if the non-test database is also named `bertron_test`.
    - **New test**: I added a test that involves accessing the MongoDB
    database. Previously, no tests did this. Fixes #75.
    
    ### Tangential changes
    - **Fix bug involving silent failures**: I fixed a bug in the GitHub
    Actions workflow, where the status code returned by the `test` container
    was not being propagated to the `docker compose` command, itself. With
    that bug in place, even if a test had failed, the GHA step would have
    succeeded. Now, GHA "sees" that status code, so the step fails when a
    test fails.
    - **Speed up container image builds**: I created a `.dockerignore` file
    so Docker omits some unnecessary directories from the container image it
    builds. I did this while working on the GHA workflow (see next bullet
    point).
    - **Avoiding `.venv/` collisions between host and container**: I updated
    the GHA workflow to run the tests in the `test` container only, instead
    of also running them locally. That eliminated the need to handle
    collisions between the `.venv/` directory created by the containers and
    the one the local `uv` program tries to create. This can be un-done, in
    case team members want the GHA workflow to also run the tests locally.
    Note: Previously, the "run tests locally" step was only running tests
    defined in the top-level `tests/` directory, which was only the "hello
    world" test.
    - **Preventing redundant GHA workflow runs**: Previously, whenever
    someone would add a commit to a PR, the CI GHA workflow would run
    twice—once, because a PR was being updated, and again, because a commit
    was being pushed to any branch. Now, whenever someone adds a commit to a
    PR, the CI GHA workflow will only run once.
    
    ---------
    
    Co-authored-by: eecavanna <eecavanna@users.noreply.github.com>

---
+++ src/config.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    r"""
+    Configuration settings for the application.
+
+    Order of precedence (highest to lowest) for settings values:
+    1. Process-level environment variable definitions.
+    2. Environment variable definitions loaded from a `.env` file.
+    3. Default values defined in this class.
+
+    References:
+    - https://docs.pydantic.dev/latest/concepts/pydantic_settings/#dotenv-env-support
+    """
+
+    # Load environment variable definitions from a `.env` file, if one exists,
+    # in the current working directory. We allow the `.env` file to contain
+    # extra environment variables (beyond those modeled below), since this
+    # repository currently contains a variety of independent applications.
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    # MongoDB connection settings.
+    mongo_host: str = "localhost"
+    mongo_port: int = 27017  # note: env. var. value will be coerced into int
+    mongo_username: Optional[str] = None
+    mongo_password: Optional[str] = None
+    mongo_database: str = "bertron"
+
+
+# Instantiate a settings object that can be imported into other modules.
+settings = Settings()
--- src/tests/test_server.py
+++ src/tests/test_server.py
@@ -9,7 +9,7 @@ import pytest
 from fastapi.testclient import TestClient
 from starlette import status
 
-from models import VersionResponse
+from models import HealthResponse, VersionResponse
 from server import app
 
 
@@ -28,7 +28,17 @@ def test_root_endpoint_redirects_to_api_docs(test_client: TestClient):
 def test_version_endpoint_returns_version_response(test_client: TestClient):
     response = test_client.get("/version")
     assert response.status_code == status.HTTP_200_OK
+    body = response.json()
     # Note: This will raise a `ValidationError` if the response is not
     #       a valid `VersionResponse` (e.g. if it has extra fields or
     #       its fields' values are of an incompatible data type).
-    _ = VersionResponse(**response.json())
+    _ = VersionResponse(**body)
+
+
+def test_health_endpoint_returns_health_response(test_client: TestClient):
+    response = test_client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
+    body = response.json()
+    assert body["web_server"] is True
+    assert body["database"] is True
+    _ = HealthResponse(**body)
